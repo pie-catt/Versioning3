8d88438e9f3a4b6a8e129b18ed1f62e645060e21
Pietro Cattaneo <pieca314@gmail.com>
2018-11-06 12:22:59 +0100

    commit feat3

--- Versioning3/Program.cs
+++ Versioning3/Program.cs
@@ -12,6 +12,8 @@ namespace Versioning3
         {
             Console.WriteLine("Commit1");
             Console.WriteLine("Commit2");
+            Console.WriteLine("Commit1 feat3");
+
 
         }
     }
